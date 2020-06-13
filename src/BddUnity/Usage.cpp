7a6cca66fb60e4c93d06013ef12277b0425a4f79
Peter Halliday <pghalliday@gmail.com>
2020-06-13 16:27:26 +0200

    print usage as defines for copying

--- src/BddUnity/Usage.cpp
+++ src/BddUnity/Usage.cpp
@@ -6,6 +6,7 @@
 #endif
 
 #define PRINT_FIELD(FORMAT, OBJECT, NAME) printf(#NAME ": " FORMAT "\n", OBJECT.NAME)
+#define PRINT_FIELD_DEFINE(DEFINE, FORMAT, OBJECT, NAME) printf("#define " #DEFINE " " FORMAT "\n", OBJECT.NAME)
 
 namespace BddUnity {
   namespace Usage {
@@ -36,6 +37,11 @@ namespace BddUnity {
       PRINT_FIELD(BDD_UNITY_SIZE_T_FORMAT, params, maxSetup);
       PRINT_FIELD(BDD_UNITY_SIZE_T_FORMAT, params, maxLoop);
       printf("----------------------------\n");
+    }
+
+    void printUsage(const Memory::Interface & memory, const Runnable & runnable) {
+      printMemoryUsage(memory);
+      printDepthUsage(runnable);
     }
 
     void printMemoryUsage(const Memory::Interface & memory) {
@@ -71,9 +77,35 @@ namespace BddUnity {
       printf("-----------------------\n");
     }
 
-    void printUsage(const Memory::Interface & memory, const Runnable & runnable) {
-      printMemoryUsage(memory);
-      printDepthUsage(runnable);
+    void printUsageDefines(const Memory::Interface & memory, const Runnable & runnable) {
+      printMemoryUsageDefines(memory);
+      printDepthUsageDefines(runnable);
     }
+
+    void printMemoryUsageDefines(const Memory::Interface & memory) {
+      BddUnity::Memory::Usage usage = memory.getUsage();
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_MODULE, BDD_UNITY_SIZE_T_FORMAT, usage, maxModule);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_DESCRIBE, BDD_UNITY_SIZE_T_FORMAT, usage, maxDescribe);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_POP, BDD_UNITY_SIZE_T_FORMAT, usage, maxPop);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_TEST, BDD_UNITY_SIZE_T_FORMAT, usage, maxTest);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_IT, BDD_UNITY_SIZE_T_FORMAT, usage, maxIt);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_ASYNC_IT, BDD_UNITY_SIZE_T_FORMAT, usage, maxAsyncIt);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_CALLBACK, BDD_UNITY_SIZE_T_FORMAT, usage, maxCallback);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_ASYNC_CALLBACK, BDD_UNITY_SIZE_T_FORMAT, usage, maxAsyncCallback);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_STACKED_CALLBACK, BDD_UNITY_SIZE_T_FORMAT, usage, maxStackedCallback);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_STACKED_ASYNC_CALLBACK, BDD_UNITY_SIZE_T_FORMAT, usage, maxStackedAsyncCallback);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_SETUP, BDD_UNITY_SIZE_T_FORMAT, usage, maxSetup);
+    }
+
+    void printDepthUsageDefines(const Runnable & runnable) {
+      BddUnity::Depth::Usage usage = runnable.getUsage();
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_DEPTH, BDD_UNITY_SIZE_T_FORMAT, usage, maxDepth);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_BEFORE_EACH, BDD_UNITY_SIZE_T_FORMAT, usage, maxBeforeEach);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_ASYNC_BEFORE_EACH, BDD_UNITY_SIZE_T_FORMAT, usage, maxAsyncBeforeEach);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_AFTER_EACH, BDD_UNITY_SIZE_T_FORMAT, usage, maxAfterEach);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_ASYNC_AFTER_EACH, BDD_UNITY_SIZE_T_FORMAT, usage, maxAsyncAfterEach);
+      PRINT_FIELD_DEFINE(BDD_UNITY_MAX_LOOP, BDD_UNITY_SIZE_T_FORMAT, usage, maxLoop);
+    }
+
   }
 }
--- src/BddUnity/Usage.hpp
+++ src/BddUnity/Usage.hpp
@@ -7,9 +7,14 @@ namespace BddUnity {
   namespace Usage {
 
     void printParams(const Memory::Interface & memory);
+
+    void printUsage(const Memory::Interface & memory, const Runnable & runnable);
     void printMemoryUsage(const Memory::Interface & memory);
     void printDepthUsage(const Runnable & runnable);
-    void printUsage(const Memory::Interface & memory, const Runnable & runnable);
+
+    void printUsageDefines(const Memory::Interface & memory, const Runnable & runnable);
+    void printMemoryUsageDefines(const Memory::Interface & memory);
+    void printDepthUsageDefines(const Runnable & runnable);
 
   }
 }
