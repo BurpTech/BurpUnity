8749bbc94290a9d9ec8a3ed5a1dfa1eac1ed6733
Peter Halliday <pghalliday@gmail.com>
2020-06-22 09:41:20 +0200

    common BURP define for size_t format

--- src/BurpUnity/Usage.cpp
+++ src/BurpUnity/Usage.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "Usage.hpp"
 
-#ifndef BURP_UNITY_SIZE_T_FORMAT
-#define BURP_UNITY_SIZE_T_FORMAT "%lu"
+#ifndef BURP_SIZE_T_FORMAT
+#define BURP_SIZE_T_FORMAT "%lu"
 #endif
 
 #define PRINT_FIELD(FORMAT, OBJECT, NAME) printf(#NAME ": " FORMAT "\n", OBJECT.NAME)
@@ -16,26 +16,26 @@ namespace BurpUnity {
       printf("----------------------------\n");
       printf("BurpUnity::Memory parameters:\n");
       printf("----------------------------\n");
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, labelBufferSize);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, labelBufferSize);
       PRINT_FIELD("%ld", params, defaultTimeout);
       PRINT_FIELD("%d", params, safeMemPools);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxModule);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxDepth);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxDescribe);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxPop);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxTest);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxIt);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxAsyncIt);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxCallback);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxAsyncCallback);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxStackedCallback);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxStackedAsyncCallback);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxBeforeEach);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxAsyncBeforeEach);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxAfterEach);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxAsyncAfterEach);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxSetup);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, params, maxLoop);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxModule);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxDepth);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxDescribe);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxPop);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxTest);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxIt);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxAsyncIt);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxCallback);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxAsyncCallback);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxStackedCallback);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxStackedAsyncCallback);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxBeforeEach);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxAsyncBeforeEach);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxAfterEach);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxAsyncAfterEach);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxSetup);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, params, maxLoop);
       printf("----------------------------\n");
     }
 
@@ -49,17 +49,17 @@ namespace BurpUnity {
       printf("-----------------------\n");
       printf("BurpUnity::Memory::Usage:\n");
       printf("-----------------------\n");
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxModule);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxDescribe);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxPop);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxTest);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxIt);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxAsyncIt);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxCallback);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxAsyncCallback);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxStackedCallback);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxStackedAsyncCallback);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxSetup);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxModule);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxDescribe);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxPop);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxTest);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxIt);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxAsyncIt);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxCallback);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxAsyncCallback);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxStackedCallback);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxStackedAsyncCallback);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxSetup);
       printf("-----------------------\n");
     }
 
@@ -68,12 +68,12 @@ namespace BurpUnity {
       printf("-----------------------\n");
       printf("BurpUnity::Depth::Usage:\n");
       printf("-----------------------\n");
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxDepth);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxBeforeEach);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxAsyncBeforeEach);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxAfterEach);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxAsyncAfterEach);
-      PRINT_FIELD(BURP_UNITY_SIZE_T_FORMAT, usage, maxLoop);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxDepth);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxBeforeEach);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxAsyncBeforeEach);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxAfterEach);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxAsyncAfterEach);
+      PRINT_FIELD(BURP_SIZE_T_FORMAT, usage, maxLoop);
       printf("-----------------------\n");
     }
 
@@ -84,27 +84,27 @@ namespace BurpUnity {
 
     void printMemoryUsageDefines(const Memory::Interface & memory) {
       BurpUnity::Memory::Usage usage = memory.getUsage();
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_MODULE, BURP_UNITY_SIZE_T_FORMAT, usage, maxModule);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_DESCRIBE, BURP_UNITY_SIZE_T_FORMAT, usage, maxDescribe);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_POP, BURP_UNITY_SIZE_T_FORMAT, usage, maxPop);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_TEST, BURP_UNITY_SIZE_T_FORMAT, usage, maxTest);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_IT, BURP_UNITY_SIZE_T_FORMAT, usage, maxIt);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_ASYNC_IT, BURP_UNITY_SIZE_T_FORMAT, usage, maxAsyncIt);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_CALLBACK, BURP_UNITY_SIZE_T_FORMAT, usage, maxCallback);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_ASYNC_CALLBACK, BURP_UNITY_SIZE_T_FORMAT, usage, maxAsyncCallback);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_STACKED_CALLBACK, BURP_UNITY_SIZE_T_FORMAT, usage, maxStackedCallback);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_STACKED_ASYNC_CALLBACK, BURP_UNITY_SIZE_T_FORMAT, usage, maxStackedAsyncCallback);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_SETUP, BURP_UNITY_SIZE_T_FORMAT, usage, maxSetup);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_MODULE, BURP_SIZE_T_FORMAT, usage, maxModule);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_DESCRIBE, BURP_SIZE_T_FORMAT, usage, maxDescribe);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_POP, BURP_SIZE_T_FORMAT, usage, maxPop);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_TEST, BURP_SIZE_T_FORMAT, usage, maxTest);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_IT, BURP_SIZE_T_FORMAT, usage, maxIt);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_ASYNC_IT, BURP_SIZE_T_FORMAT, usage, maxAsyncIt);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_CALLBACK, BURP_SIZE_T_FORMAT, usage, maxCallback);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_ASYNC_CALLBACK, BURP_SIZE_T_FORMAT, usage, maxAsyncCallback);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_STACKED_CALLBACK, BURP_SIZE_T_FORMAT, usage, maxStackedCallback);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_STACKED_ASYNC_CALLBACK, BURP_SIZE_T_FORMAT, usage, maxStackedAsyncCallback);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_SETUP, BURP_SIZE_T_FORMAT, usage, maxSetup);
     }
 
     void printDepthUsageDefines(const Runnable & runnable) {
       BurpUnity::Depth::Usage usage = runnable.getUsage();
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_DEPTH, BURP_UNITY_SIZE_T_FORMAT, usage, maxDepth);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_BEFORE_EACH, BURP_UNITY_SIZE_T_FORMAT, usage, maxBeforeEach);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_ASYNC_BEFORE_EACH, BURP_UNITY_SIZE_T_FORMAT, usage, maxAsyncBeforeEach);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_AFTER_EACH, BURP_UNITY_SIZE_T_FORMAT, usage, maxAfterEach);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_ASYNC_AFTER_EACH, BURP_UNITY_SIZE_T_FORMAT, usage, maxAsyncAfterEach);
-      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_LOOP, BURP_UNITY_SIZE_T_FORMAT, usage, maxLoop);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_DEPTH, BURP_SIZE_T_FORMAT, usage, maxDepth);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_BEFORE_EACH, BURP_SIZE_T_FORMAT, usage, maxBeforeEach);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_ASYNC_BEFORE_EACH, BURP_SIZE_T_FORMAT, usage, maxAsyncBeforeEach);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_AFTER_EACH, BURP_SIZE_T_FORMAT, usage, maxAfterEach);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_ASYNC_AFTER_EACH, BURP_SIZE_T_FORMAT, usage, maxAsyncAfterEach);
+      PRINT_FIELD_DEFINE(BURP_UNITY_MAX_LOOP, BURP_SIZE_T_FORMAT, usage, maxLoop);
     }
 
   }
